c9b9b8a4ec9da8f23b5fc1e514147513dfbe3b72
alex drozdovs <alex.drozdovs@gmail.com>
2014-05-28 20:37:59 +0400

    ?? ????????????? ?????? view_results ?????? ????????????? ?????

--- view_results/__init__.py
+++ view_results/__init__.py
@@ -1,2 +1,2 @@
 
-__all__ = ['view_results', 'view_signal', 'plotter', 'view_wavelet', 'view_wavelet_scales', 'compare_wavelet_scales', 'compare_plotter', 'view_imitation', 'plotter_3d', 'view_chvo', 'objview', 'matrix_view', 'plotter_gnuplot']
+__all__ = ['view_results', 'view_signal', 'plotter', 'view_wavelet', 'view_wavelet_scales', 'compare_wavelet_scales', 'compare_plotter', 'plotter_3d', 'objview', 'matrix_view', 'plotter_gnuplot']
