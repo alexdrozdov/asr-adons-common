243f2256ecd956fa0726351e7fa20f387b6d8a6b
alex drozdovs <alex.drozdovs@gmail.com>
2014-09-14 21:01:54 +0400

    ????????? ????????? ????????? ????? ?????? ? ???? ?? udp

--- sendmsg/sendmsg.py
+++ sendmsg/sendmsg.py
@@ -6,6 +6,7 @@ import wx, sendmsg_interface
 import traceback
 import localdb
 import copy
+import adon_window
 
 class RemoteHostConnectionInfo:
     def __init__(self, params):
@@ -45,27 +46,77 @@ class UdpConnectionInfo(RemoteHostConnectionInfo):
         if self.params["port"]<1 or self.params["port"]>65535:
             raise ValueError(u"Номер udp-порта выходит за допустимые границы 1...65535")
 
-class SendMsgInterface(sendmsg_interface.SendMessageInterface):
+class RcvPort(object):
+    def __init__(self, params):
+        self.params = params
+        self.enabled = True
+    def get_channel(self):
+        pass
+    def get_channel_params(self):
+        pass
+    def set_channel_params(self, param_string):
+        pass
+    def set_enabled(self, en):
+        self.enabled = en
+    def get_enabled(self):
+        return self.enabled
+    
+    
+class UdpRcvPort(RcvPort):
+    def __init__(self, params=None):
+        if None==params:
+            params={"port":1000}
+        RcvPort.__init__(self, params)
+    def get_channel(self):
+        return "udp"
+    def get_channel_params(self):
+        return str(self.params["port"])
+    def set_channel_params(self, param_string):
+        self.params["port"] = int(param_string)
+        if self.params["port"]<1 or self.params["port"]>65535:
+            raise ValueError(u"Номер udp-порта выходит за допустимые границы 1...65535")
+
+class SendMsgInterface(sendmsg_interface.SendMessageInterface, adon_window.AdonWindow):
     def __init__(self, manager):
         sendmsg_interface.SendMessageInterface.__init__(self, None, -1, "")
+        adon_window.AdonWindow.__init__(self, window_id='wnd_sndmsg', window_caption='')
         self.Bind(wx.EVT_SHOW, self.on_show, self)
         self.Bind(wx.EVT_CLOSE, self.on_form_close, self)
         self.Bind(wx.grid.EVT_GRID_CMD_CELL_CHANGE, self.gridHosts_on_cell_changed, self.gridHosts)
+        self.Bind(wx.grid.EVT_GRID_CMD_CELL_CHANGE, self.gridRcvPorts_on_cell_changed, self.gridRcvPorts)
         self.man = manager
         self.remote_hosts = []
+        self.rcv_ports = []
 
     def on_form_close(self, event):
+        adon_window.AdonWindow.on_form_close(self, event)
         self._load_from_localdb()
         self.Hide()
     def _fill_msglist_combo(self, combo):
         for k in self.man.handlers.keys():
             combo.Append(str(self.man.dis.get_name_by_id(k)))
     def on_show(self, evt):
-        if evt.GetShow():
+        show = evt.GetShow()
+        adon_window.AdonWindow.on_form_show(self, evt)
+        if show:
             self._load_from_localdb()
             self._fill_msglist_combo(self.comboSendMessages)
-            self._fill_msglist_combo(self.comboRcvMessages)
             self._fill_hosts_list()
+            self._fill_rcv_list()
+    def _fill_rcv_list(self):
+        try:
+            try:
+                self.gridRcvPorts.DeleteRows(0, self.gridRcvPorts.GetNumberRows(), True)
+            except:
+                pass
+            r_cnt = 0
+            for p in self.rcv_ports:
+                self.gridRcvPorts.AppendRows(1, True)
+                self.gridRcvPorts.SetCellValue(r_cnt, 0, p.get_channel())
+                self.gridRcvPorts.SetCellValue(r_cnt, 1, p.get_channel_params())
+                r_cnt += 1
+        except:
+            print traceback.format_exc()
                         
     def _fill_hosts_list(self):
         try:
@@ -89,22 +140,6 @@ class SendMsgInterface(sendmsg_interface.SendMessageInterface):
         self.listSendMessages.InsertColumn(0, u"Типы сообщений", width=300)
         for m in host.messages_to_send:
             self.listSendMessages.Append([m])
-    
-    def btLoadMsgConfig_on_click(self, event): # wxGlade: SendMessageInterface.<event_handler>
-        print "Event handler `btLoadMsgConfig_on_click' not implemented!"
-        event.Skip()
-
-    def btnExportMsgConfig_on_click(self, event): # wxGlade: SendMessageInterface.<event_handler>
-        print "Event handler `btnExportMsgConfig_on_click' not implemented!"
-        event.Skip()
-
-    def comboMsgConfigs_on_select(self, event): # wxGlade: SendMessageInterface.<event_handler>
-        print "Event handler `comboMsgConfigs_on_select' not implemented!"
-        event.Skip()
-
-    def btnEditConfig_on_click(self, event): # wxGlade: SendMessageInterface.<event_handler>
-        print "Event handler `btnEditConfig_on_click' not implemented!"
-        event.Skip()
 
     def gridHosts_on_select(self, event): # wxGlade: SendMessageInterface.<event_handler>
         try:
@@ -192,31 +227,57 @@ class SendMsgInterface(sendmsg_interface.SendMessageInterface):
             self._fill_host_messages_list(host)
         except:
             print traceback.format_exc()
+        
+    def btnAddRcvPort_on_click(self, event):
+        self.rcv_ports.append(UdpRcvPort())
+        self._fill_rcv_list()
 
-    def comboRcvMessages_on_select(self, event): # wxGlade: SendMessageInterface.<event_handler>
-        print "Event handler `comboRcvMessages_on_select' not implemented!"
-        event.Skip()
+    def btnRemoveRcvPort_on_click(self, event): # wxGlade: SendMessageInterface.<event_handler>
+        try:
+            sel_rows = self.gridRcvPorts.GetSelectedRows()
+            rh = []
+            for r_cnt in range(len(self.rcv_ports)):
+                if r_cnt in sel_rows:
+                    continue
+                rp.append(self.rcv_ports[r_cnt])
+            self.rcv_ports = rp
+            self._fill_rcv_list()
+        except:
+            print traceback.format_exc()
 
-    def btnAddRcvMessage_on_click(self, event): # wxGlade: SendMessageInterface.<event_handler>
-        print "Event handler `btnAddRcvMessage_on_click' not implemented!"
+    def btnDisableRcvPort_on_click(self, event): # wxGlade: SendMessageInterface.<event_handler>
+        print "Event handler `btnDisableRcvPort_on_click' not implemented!"
         event.Skip()
-
-    def btnRemoveRcvMessage_on_click(self, event): # wxGlade: SendMessageInterface.<event_handler>
-        print "Event handler `btnRemoveRcvMessage_on_click' not implemented!"
-        event.Skip()
+        
+    def gridRcvPorts_on_select(self, event): # wxGlade: SendMessageInterface.<event_handler>
+        pass
+    
+    def gridRcvPorts_on_cell_changed(self, event): # wxGlade: UdpConfigInterface.<event_handler>
+        try:
+            r = event.GetRow()
+            h = self.rcv_ports[r]
+            h.set_channel_params(self.gridRcvPorts.GetCellValue(r, 1))
+        except:
+            print traceback.format_exc()
 
     def btnCancel_on_click(self, event): # wxGlade: SendMessageInterface.<event_handler>
         self.Hide()
     
     def _save_to_localdb(self):
         localdb.db.write_persistent("/db/persistent/sendmsg/localhost/remote_hosts", self.remote_hosts)
+        localdb.db.write_persistent("/db/persistent/sendmsg/localhost/rcv_ports", self.rcv_ports)
     
     def _load_from_localdb(self):
         try:
             self.remote_hosts = localdb.db.read_value("/db/persistent/sendmsg/localhost/remote_hosts")
             self.remote_hosts = copy.deepcopy(self.remote_hosts)
         except:
-            pass
+            self.remote_hosts = []
+        try:
+            self.rcv_ports = localdb.db.read_value("/db/persistent/sendmsg/localhost/rcv_ports")
+            self.rcv_ports = copy.deepcopy(self.rcv_ports)
+        except:
+            self.rcv_ports = []
 
     def btnSaveToBase_on_click(self, event): # wxGlade: SendMessageInterface.<event_handler>
         try:
--- sendmsg/sndrcv.py
+++ sendmsg/sndrcv.py
@@ -1,17 +1,129 @@
  #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import socket
+import pickle
 import traceback
 import localdb
+import time
+import select
+import thread
 
-class SndDaemon:
+class SndRouter(object):
+    def __init__(self, sock, manager, data_id):
+        self.sock = sock
+        self.data_id = data_id
+        self.man = manager
+        self.man.register_handler(data_id, self.ticket_handler)
+        self.remote_hosts = []
+    def add_remote_host(self, addr_port):
+        self.remote_hosts.append(addr_port)
+    def ticket_handler(self, ticket):
+        ticket = ticket.export_standalone_ticket()
+        pickled_ticket = pickle.dumps(ticket)
+        for h in self.remote_hosts:
+            self.sock.sendto(pickled_ticket, h)
+class SndDaemon(object):
     def __init__(self, manager):
-        pass
+        self.man = manager
+        remote_hosts = None
+        try:
+            remote_hosts = localdb.db.read_value("/db/persistent/sendmsg/localhost/remote_hosts")
+        except:
+            return
+        self.routers = self.__generate_msg_routers(remote_hosts)
+    def __build_sndmsg_list(self, remote_hosts):
+        msglist = []
+        for rh in remote_hosts:
+            for m in rh.messages_to_send:
+                if m in msglist:
+                    continue
+                msglist.append(m)
+        return msglist
+    def __generate_msg_routers(self, remote_hosts):
+        self.routers = []
+        msg_list = self.__build_sndmsg_list(remote_hosts)
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        for m in msg_list:
+            router = SndRouter(self.sock, self.man, m)
+            hosts = [(rhci.get_address(), int(rhci.get_channel_params())) for rhci in remote_hosts if m in rhci.messages_to_send ]
+            for h in hosts:
+                router.add_remote_host(h)
+
+class UdpReceiver:
+    def __init__(self, manager, port):
+        self.man = manager
+        self.port = port
+        self.fd = None
+        self.open_port()
+    def open_port(self):
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.sock.bind(('', self.port))
+        self.sock.setblocking(False)
+        self.fd = self.sock.fileno()
+    def get_fd(self):
+        return self.fd
+    def get_socket(self):
+        return self.sock
+    def read_socket(self):
+        data, addr = self.sock.recvfrom(65000)
+        try:
+            remote_ticket = pickle.loads(data)
+            ticket = self.man.ticket(remote_ticket.get_data_name(),
+                                            remote_ticket.get_data(), description = remote_ticket.description)
+            self.man.push_ticket(ticket)
+        except:
+            print traceback.format_exc()
+        #self.man.push_ticket(self.man.ticket(self.data_id, data, self.ticket_name))
+class UdpPoller:
+    def __init__(self):
+        self.p = select.poll()
+        self.fd_to_receiver = {}
+        self.enabled = False
+        self.is_running = False
+    def add_udp_receiver(self, udp_receiver):
+        self.p.register(udp_receiver.get_socket(), select.POLLIN | select.POLLHUP)
+        self.fd_to_receiver[udp_receiver.get_fd()] = udp_receiver
+    def __poll(self, timeout):
+        events = self.p.poll(timeout)
+        for fd, flag in events:
+            ur = self.fd_to_receiver[fd]
+            ur.read_socket()
+    def __poll_thread(self):
+        self.is_running = True
+        try:
+            while self.enabled:
+                self.__poll(1000)
+        except:
+            print traceback.format_exc()
+        self.is_running = False
+    def start_async_operation(self):
+        self.enabled = True
+        self.is_running = False
+        self.rcv_thread = thread.start_new_thread(self.__poll_thread, ())
+    def stop_async_operation(self):
+        self.enabled = False
+        while self.is_running:
+            time.sleep(1)
+    def clear(self):
+        self.stop_async_operation()
+        for fd in self.fd_to_receiver.keys():
+            self.p.unregister(fd)
+
 
 class RcvDaemon:
     def __init__(self, manager):
-        pass
-
+        self.man = manager
+        rcv_ports = None
+        self.udp_poller = UdpPoller()
+        try:
+            rcv_ports = localdb.db.read_value("/db/persistent/sendmsg/localhost/rcv_ports")
+        except:
+            return
+        for r in rcv_ports:
+            ur = UdpReceiver(self.man, int(r.get_channel_params(),0))
+            self.udp_poller.add_udp_receiver(ur)
+        self.udp_poller.start_async_operation()
 
 def init_module(manager, gui):
     s = SndDaemon(manager)
